0655fe7da6fbed3d3966340054a217f4edeb66a1
EclecticTaco <calvi5210@gmail.com>
2020-02-05 12:03:25 -0800

    stack function passes all stack test except for sequentially add

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[value] = counter;
+    counter += 1;
   };
 
   someInstance.pop = function() {
+    for (var key in storage) {
+      if (storage[key] === (counter - 1)) {
+        delete storage[key];
+      }
+    }
   };
 
   someInstance.size = function() {
+    var count = 0;
+    for (var key in storage) {
+      count += 1;
+    }
+    return count;
   };
 
   return someInstance;
 };
+
+// stack returns somInstance, an object, its keys are (push, pop, size), the values are functions attched to the keys
