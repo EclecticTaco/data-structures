1a4b49e23b4f165ba51bd03aafd1e7739f60e3fb
EclecticTaco <calvi5210@gmail.com>
2020-02-06 13:28:16 -0800

    ES6 passes test for stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,28 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(Stacks) {
+    this.storage = {};
+    this.counter = 0;
+  }
+  push(value) {
+    this.storage[this.counter] = value;
+    this.counter += 1;
   }
 
+  pop() {
+    var temp;
+    temp = this.storage[this.counter - 1];
+    delete this.storage[this.counter - 1];
+    this.counter -= 1;
+    return temp;
+  }
+
+  size() {
+    var count = 0;
+    for (var key in this.storage) {
+      count += 1;
+    }
+    return count;
+  }
 }
