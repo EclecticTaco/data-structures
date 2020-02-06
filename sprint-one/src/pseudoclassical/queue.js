ebca50f5b5445f63b3fab044905c9ce0050f98d4
EclecticTaco <calvi5210@gmail.com>
2020-02-06 13:21:00 -0800

    solved all test cases for pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.topCount = 0;
+  this.botCount = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.topCount] = value;
+  this.topCount += 1;
+};
+
+Queue.prototype.dequeue = function() {
+  var temp = this.storage[this.botCount];
+  delete this.storage[this.botCount];
+  this.botCount += 1;
+  return temp;
+};
+
+Queue.prototype.size = function() {
+  var count = 0;
+  for (var key in this.storage) {
+    count += 1;
+  }
+  return count;
+};
+
 
 
