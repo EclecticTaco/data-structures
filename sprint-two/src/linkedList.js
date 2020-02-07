4685df72b64a92ec028bceefd7d9f484f30dd4be
EclecticTaco <calvi5210@gmail.com>
2020-02-06 18:27:35 -0800

    LinkedList passes addToTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,33 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.previous = null;
 
   list.addToTail = function(value) {
+    if (this.tail === null) { // if list is empty run
+      this[value] = Node(value); // create new node with pass in value
+      this.head = this[value]; // link head points to above node
+      this.tail = this[value]; // link tail points to above node
+      this.previous = value; // sets previous to current value
+    } else {
+      this[value] = Node(value); // if list not empty run
+      this.tail = this[value]; // set tail to this new node
+      var before = this.previous;
+      this[before].next = value; // set previous node's next value to newly added node
+      this[before] = value; // set previous current (over writing old previous)
+    }
+    console.log(this.head.next);
   };
 
   list.removeHead = function() {
+    var oldHeader = this.head;
+    var oldNext = this[oldHeader].next;
+    // var secondNode = this[oldHeader].next;
+    // this.head = this[secondNode];
+
+
+
+    // return oldHeader;
   };
 
   list.contains = function(target) {
