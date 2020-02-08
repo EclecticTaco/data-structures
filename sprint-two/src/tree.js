73512c9758d904317c967be76a137e4fba756b29
EclecticTaco <calvin5210@gmail.com>
2020-02-07 21:24:52 -0800

    tree: pass all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,34 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  var child = this;
+  var inner = function(tree) {
+    if (tree.value === target) {
+      return true;
+    }
+    for (var i = 0; i < tree.children.length; i ++) {
+      var call = inner(tree.children[i]);
+      if (call === true) {
+        return true;
+      }
+    }
+    return false;
+  };
+  return inner(child);
+
 };
 
 
@@ -21,3 +38,11 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var tree = Tree(20);
+
+tree.addChild(5);
+tree.addChild(6);
+tree.children[0].addChild(7);
+tree.children[1].addChild(8);
+tree.contains(7);
+tree.contains(8);
