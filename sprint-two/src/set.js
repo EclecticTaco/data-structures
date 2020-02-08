7dc827e0428ecb140ff484eeffa593b7fbe17c01
EclecticTaco <calvin5210@gmail.com>
2020-02-08 12:23:01 -0800

    Set adds values, removes values, and checks if contains values

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this.storage) {
+    if (item === key) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (var key in this.storage) {
+    if (item === key) {
+      delete this.storage[key];
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var set = Set();
+set.add('Mel Gibson');
+set.remove('Mel Gibson');
+set.contains('Mel Gibson')
