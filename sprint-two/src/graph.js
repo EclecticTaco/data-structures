5073e929b37e04f4981a35ef7b11379f9faf2d74
EclecticTaco <calvin5210@gmail.com>
2020-02-08 11:21:50 -0800

    graph passes tests for add node/ remove edge/ add edge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,26 +2,65 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.graphs = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  var newNode = {};
+  newNode.value = node;
+  newNode.storage = [];
+
+  this.graphs.push(newNode);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for (var i = 0; i < this.graphs.length; i += 1) {
+    if (node === this.graphs[i].value) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.graphs.length; i += 1) {
+    if (this.graphs[i].value === node) {
+      this.graphs.splice(i, 1);
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var i = 0; i < this.graphs.length; i += 1) {
+    if (this.graphs[i].value === fromNode) {
+      for (var x = 0; x < this.graphs[i].storage.length; x += 1) {
+        if (this.graphs[i].storage[x].value === toNode) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  var from = undefined;
+  var to = undefined;
+  for (var i = 0; i < this.graphs.length; i++ ) {
+    if (this.graphs[i].value === fromNode) {
+      from = this.graphs[i];
+    }
+    if (this.graphs[i].value === toNode) {
+      to = this.graphs[i];
+    }
+  }
+  from.storage.push(to);
+  to.storage.push(from);
 };
 
 // Remove an edge between any two specified (by value) nodes.
@@ -36,4 +75,11 @@ Graph.prototype.forEachNode = function(cb) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+var bob = new Graph();
+bob.addNode(2);
+bob.addNode(1);
+bob.addNode(3);
+bob.addEdge(3, 2);
+bob.hasEdge(3, 2);
+bob.hasEdge(3, 1);
 
