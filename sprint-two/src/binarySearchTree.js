512fe71d435f59056ad834d606f61cc90fbe7f21
EclecticTaco <calvi5210@gmail.com>
2020-02-09 12:21:41 -0800

    basic skeleton created

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,25 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(treeMethods);
+  this.value = value;
+  this.left = undefined;
+  this.right = undefined;
+
+  return tree;
 };
 
+var treeMethods = {
+  insert: function(value) {
 
+  },
+
+  contains: function(value) {
+
+  },
+
+  depthFirstLog: function(cb) {
+
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
