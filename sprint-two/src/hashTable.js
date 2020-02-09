02a82df7386ca16a065fa934c0cf1f7484b8fe21
EclecticTaco <calvin5210@gmail.com>
2020-02-08 16:10:15 -0800

    progress on HashTable, adds but cannot retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,14 +3,23 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  // this._storage.storage.length = 8;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, [k, v]);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var array = this._storage.get(index);
+  for (var i = 0; i < array.length; i++) {
+    if (array[i][0] === k) {
+      return array[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
